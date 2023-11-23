fa1f404453995bca67f0eb9fe86cab747be66fa1
Yasser Ahmed <yasser.luxor2016@gmail.com>
2023-11-23 22:22:51 +0200

    Basic Data Types added

---
+++ Sheet #1 (Data type - Conditions)/BasicDataTypes/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var string= readLine()!!.split(" ");
+    println(string[0]);
+    println(string[1]);
+    println(string[2]);
+    println(string[3]);
+    println(string[4]);
+
+}
