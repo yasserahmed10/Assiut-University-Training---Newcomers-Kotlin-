4b1f3dbfca64ebd1cc02075a284f7d609dc3d86d
Yasser Ahmed <yasser.luxor2016@gmail.com>
2023-11-30 11:13:13 +0200

    The Brothers added

---
+++ Sheet #1 (Data type - Conditions)/The Brothers/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val pesron1= readln().split(" ")
+    val pesron2= readln().split(" ")
+    val f1=pesron1[0]
+    val s1=pesron1[1]
+    val f2=pesron2[0]
+    val s2=pesron2[1]
+    if (s1==s2)
+    {
+       println("ARE Brothers")
+    }
+    else
+    {
+       println("NOT")
+    }
+}
