88add4185dfd2f89811b50905be4080b9d2b5a11
Yasser Ahmed <yasser.luxor2016@gmail.com>
2023-12-01 20:06:42 +0200

    Char added

---
+++ Sheet #1 (Data type - Conditions)/Char/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val input = readLine()
+    if (input==input!!.lowercase())
+    {
+        println(input.uppercase())
+    }
+    else if (input==input.uppercase())
+    {
+        println(input.lowercase())
+    }
+
+}
