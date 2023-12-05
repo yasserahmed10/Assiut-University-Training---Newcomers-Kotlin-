044e5c12f44e5fc8edeea5324b13a4a3e1246dd6
Yasser Ahmed <yasser.luxor2016@gmail.com>
2023-12-05 23:12:26 +0200

    Hard Compare Updated

--- Sheet #1 (Data type - Conditions)/Hard Compare/src/main/kotlin/Main.kt
+++ Sheet #1 (Data type - Conditions)/Hard Compare/src/main/kotlin/Main.kt
@@ -1,11 +1,12 @@
-import java.lang.Math.*
+import kotlin.math.ln
+
 fun main() {
     val inputs= readln().split(" ")
     val num1=inputs[0].toDouble()
     val num2=inputs[1].toDouble()
     val num3=inputs[2].toDouble()
     val num4=inputs[3].toDouble()
-    if (num2*log(num1)>num4*log(num3))
+    if (num2* ln(num1) >num4* ln(num3))
     {
        println("YES")
     }
