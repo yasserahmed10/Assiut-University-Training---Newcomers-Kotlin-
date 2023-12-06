030063deeef456b0ab1c6e65f39d083c70064d48
Yasser Ahmed <yasser.luxor2016@gmail.com>
2023-12-06 21:07:03 +0200

    Adding Bits upated

--- Contest #1/Adding Bits/src/main/kotlin/Main.kt
+++ Contest #1/Adding Bits/src/main/kotlin/Main.kt
@@ -2,6 +2,5 @@ fun main() {
     val inputs= readln().split(" ")
     val num1=inputs[0].toInt()
     val num2=inputs[1].toInt()
-   // val addingBits = num1.xor(num2)
     println(num1.xor(num2))
 }
